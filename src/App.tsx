4f1b366679ad95b79302b9e731ae94ed75f22482
WilliamWjd <william.jose.wjd@gmail.com>
2022-11-23 09:09:18 -0300

    feat: configurando o react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { SignIn } from "./pages/SignIn";
 import { GlobalStyle } from "./styles/GlobalStyles";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export function App() {
   return (
     <>
+      <ToastContainer />
       <GlobalStyle />
       <SignIn />
     </>
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import { FiMail, FiLock } from 'react-icons/fi';
 import { useForm } from 'react-hook-form';
 import { Container, Content, Form } from "./styles";
+import { toast } from 'react-toastify';
 
 import imageSignIn from '../../images/imageSignIn.svg';
 
@@ -8,11 +9,20 @@ import { Input } from "../../components/Input";
 import { Button } from '../../components/Button';
 import { useRef } from 'react';
 
+interface DataProps {
+    email: string;
+    passwor: string;
+}
+
 export function SignIn() {
     const { register, handleSubmit, formState: { errors } } = useForm();
     const formRef = useRef();
 
     function onSubmit(data: any) {
+        if (data.email === '' || data.senha === '') {
+            console.log('entrou aqui')
+            toast.error("Usuário ou senha inválido")
+        }
         console.log(data)
     }
 
